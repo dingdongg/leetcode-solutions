2b6a08d84fea6e48b60c640403972a14998ccf00
dingdongg <cs.dg.kim@gmail.com>
2023-07-06 23:43:25 -0700

    create script to mark the problems solved

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # LEETCODE SOLUTIONS
 
 So that I can keep track of progress and use my own code editor for the solution :)
+
+## Timeline
+
+Wed, Jul 5, 2023 - Solved #424
+Wed, Jul 5, 2023 - Solved #567
+Thu, Jul 6, 2023 - Solved #76
---
+++ solved.py
@@ -0,0 +1,26 @@
+import sys
+from datetime import datetime
+
+months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+]
+
+dayOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+def mark_solved(problem_num: int):
+    today = datetime.now()
+    day = dayOfWeek[today.weekday()]
+    month = months[today.month - 1]
+    date = today.day
+    year = today.year
+    today_formatted = f"{day}, {month} {date}, {year}"
+    with open("README.md", "a") as file:
+        file.write(f"{today_formatted} - Solved #{problem_num}\n")
+        file.close()
+
+if __name__ == "__main__":
+    if type(int(sys.argv[1])) is int:
+        mark_solved(int(sys.argv[1]))
+    else:
+        print("Enter an integral problem number")
