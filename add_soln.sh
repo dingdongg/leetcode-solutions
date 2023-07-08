361ef241da40eb97814b1bc3806f793782bb202d
dingdongg <cs.dg.kim@gmail.com>
2023-07-08 15:31:33 -0700

    fix error handling and directory setup

--- add_soln.sh
+++ add_soln.sh
@@ -1,18 +1,27 @@
-dir="$1"
+problem_num="$1"
 path_prefix="$PWD"
-full_path="$path_prefix/$dir"
+json_file="problems.json"
 
+# fetch title, title slug from json file and parse it
+title_slug=$(jq .[$problem_num].titleSlug $json_file | sed 's/^.//;s/.$//')
+title=$(jq .[$problem_num].title $json_file | sed 's/^.//;s/.$//')
+dirname="$problem_num-$title_slug"
+full_path="$path_prefix/$dirname"
+
+# console messages
 ERROR_DIR_EXISTS="Solution folder already exists"
-SUCCESS_MSG="Solution folder for $dir created"
+SUCCESS_MSG="Solution folder for $dirname created"
 
-if [ -d full_path ]
+if [ -d $full_path ]
 then
     echo $ERROR_DIR_EXISTS
 else
     mkdir $full_path
     cd $full_path
-    cp $path_prefix/reflection_template.md README.md
+    touch README.md 
+    echo "# $problem_num - $title\n" >> README.md
+    cat $path_prefix/reflection_template.md >> README.md
     touch main.py
     echo $SUCCESS_MSG
-    code $full_path
+    code $full_path 
 fi
