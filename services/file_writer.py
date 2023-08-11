c3c50e103c538df3f67ebf11f1b19717f67bb0d5
dingdongg <cs.dg.kim@gmail.com>
2023-08-11 01:37:31 -0700

    add comments

--- services/file_writer.py
+++ services/file_writer.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 from services.questions import Question
 
+# CONSTANTS
+
 months = [
     "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
@@ -19,6 +21,12 @@ diff_emojis = {
 
 class FileWriterService:
     def append_to_file(path: str, text: str):
+        """Append `text` to the specified file at `path`
+
+        Args:
+            path (str): Relative path to the file
+            text (str): Text to append
+        """
         with open(path, "a") as file:
             file.write(text)
             file.close()
@@ -28,6 +36,16 @@ class FileWriterService:
         m = datetime.now().month,
         d = datetime.now().day,
     ) -> str:
+        """Returns the specified date in the format '<day>, <month> <date> <year>'
+
+        Args:
+            y (int, optional): Custom year. Defaults to datetime.now().year.
+            m (int, optional): Custom month. Defaults to datetime.now().month.
+            d (int, optional): Custom day. Defaults to datetime.now().day.
+
+        Returns:
+            str: Formatted date
+        """
         desired_date = datetime(y, m, d)
         day = dayOfWeek[desired_date.weekday()]
         month = months[m - 1]
@@ -35,6 +53,14 @@ class FileWriterService:
         return f"{day}, {month} {d}, {y}"
 
     def get_problem_link(question: Question) -> str:
+        """Generate an HTML <a/> tag that links to the LC question
+
+        Args:
+            question (Question): LC question
+
+        Returns:
+            str: HTML <a/> tag that links to the LC question
+        """
         problem_url = base_url + question.titleSlug
         hyperlink_text = f"#{question.id} - {question.title}"
 
@@ -44,6 +70,15 @@ class FileWriterService:
         attempted: bool, 
         solved_q: Question,
     ) -> str:
+        """Generate and return a message indicating that `solved_q` was solved
+
+        Args:
+            attempted (bool): Whether or not this is a re-attempt
+            solved_q (Question): Question solved
+
+        Returns:
+            str: Message indicating that `solved_q` was solved
+        """
         date_formatted = FileWriterService.get_formatted_date()
         hyperlink = FileWriterService.get_problem_link(solved_q)
 
@@ -53,6 +88,11 @@ class FileWriterService:
         return f"\n{difficulty_emoji} {date_formatted}: {action} {hyperlink}\n"
 
     def mark_as_retry(question: Question):
+        """Add `question` to the retry list
+
+        Args:
+            question (Question): Question to retry
+        """
         days_offset = 3
         diff_emoji = diff_emojis[question.difficulty.lower()]
         date = FileWriterService.get_formatted_date(d=datetime.now().day + days_offset)
@@ -64,6 +104,12 @@ class FileWriterService:
         print(f"Do problem #{question.title} again in {days_offset} days!")
 
     def mark_solved(question: Question, attempted: bool):
+        """Mark a question as solved
+
+        Args:
+            question (Question): Question solved
+            attempted (bool): Whether or not this was a re-attempt
+        """
         msg_to_append = FileWriterService.generate_message(attempted, question)
 
         FileWriterService.append_to_file("README.md", msg_to_append)
