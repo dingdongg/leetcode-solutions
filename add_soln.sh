f11ac1bf5cde3ff27714cdf87c4c84ddb8a59b79
dingdongg <cs.dg.kim@gmail.com>
2023-08-13 20:31:03 -0700

    add non-integer checks

--- add_soln.sh
+++ add_soln.sh
@@ -2,6 +2,12 @@ problem_num="$1"
 path_prefix="$PWD"
 json_file="questions.json"
 
+if ! [[ $problem_num =~ '^[0-9]+$' ]]
+then
+    echo "Supply an INTEGER problem number."
+    exit
+fi
+
 # fetch title, title slug from json file and parse it
 title_slug=$(jq .[$problem_num].titleSlug $json_file | sed 's/^.//;s/.$//')
 title=$(jq .[$problem_num].title $json_file | sed 's/^.//;s/.$//')
